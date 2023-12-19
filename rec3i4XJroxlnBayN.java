4a60de3b3f9b69bbd667889de129c58813d8203f
orferr <154352729+orferr@users.noreply.github.com>
2023-12-19 17:00:07 +0100

    rec3i4XJroxlnBayN.java

---
+++ rec3i4XJroxlnBayN.java
@@ -0,0 +1,3 @@
+String phoneRegex = "\\b\\d{3}[-\\s]?\\d{4}\\b";
+String emailRegex = "^[a-zA-Z0-9._-]+@[a-zA-Z]+\\.[a-zA-Z]{2,}$";
+String passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
