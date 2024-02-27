f16a8d47c407b36254edbca3a4f50380e33e7d46
orferr <154352729+orferr@users.noreply.github.com>
2024-02-27 15:09:10 +0100

    Create BookController.java

---
+++ recFtKJOh7WyluZWE/BookController.java
@@ -0,0 +1,11 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+}
